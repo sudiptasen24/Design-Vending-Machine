02e51eed036d6bad66bd8c75fcf67f1c7a0753c8
sudipta <sudsen@microsoft.com>
2017-08-26 15:49:37 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine obj = new VendingMachine();
+            obj.Start();
+        }
+    }
+
+    public class VendingMachine
+    {
+        public VendingMachineState currentState = null;
+
+        public VendingMachine()
+        {
+           
+            currentState = new Selectproduct(this);
+        }
+        public void Start()
+        {
+            Console.WriteLine("Machine Starting Up");
+            ItemRepository.ItemInstance.FillItems();
+            while (true)
+            {
+                currentState.DisplayNextScreen();
+            }
+
+        }
+    }
+    public enum Item
+    {
+        Coke,
+        Pepsi,
+        Fanta
+    }
+
+    public class ItemRepository
+    {
+        Dictionary<Item, int> items = new Dictionary<Item, int>();
+        Dictionary<Item, int> itemsprice = new Dictionary<Item, int>();
+
+        private static ItemRepository itemrepo;
+        private ItemRepository()
+        {
+
+        }
+
+        public static ItemRepository ItemInstance
+        {
+            get
+            {
+                if (itemrepo == null)
+                {
+                    itemrepo = new ItemRepository();
+
+                }
+                return itemrepo;
+            }
+        }
+        public Dictionary<Item, int> ItemCollection
+        {
+            get { return items; }
+        }
+
+        public Dictionary<Item, int> ItemPrice
+        {
+
+            get { return itemsprice; }
+        }
+        public void FillItems()
+        {
+            items.Add(Item.Coke, 2);
+
+            items.Add(Item.Pepsi, 1);
+
+            items.Add(Item.Fanta, 3);
+
+            itemsprice.Add(Item.Coke, 20);
+            itemsprice.Add(Item.Pepsi, 10);
+            itemsprice.Add(Item.Fanta, 30);
+
+        }
+    }
+    }
---
+++ VendingMachineState.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    public abstract class VendingMachineState
+    {
+        private VendingMachine vendor;
+
+        public int AmountPaid
+        {
+            get; set;
+        }
+        public VendingMachine Vendor
+        {
+            get { return vendor; }
+            set { vendor = value; }
+        }
+        public Item selecteditem
+        {
+            get; set;
+        }
+        public int itemsprice
+        {
+            get
+            {
+                var itemsprice = ItemRepository.ItemInstance.ItemPrice;
+                return itemsprice[selecteditem];
+            }
+        }
+        public abstract void DisplayNextScreen();
+    }
+
+    public class Selectproduct : VendingMachineState
+    {
+      public Selectproduct(VendingMachine obj)
+        {
+            Vendor = obj;
+            AmountPaid = 0;
+          
+        }
+        public override void DisplayNextScreen()
+        {
+            Console.WriteLine("select item : Coke,Pepsi,Fanta");
+            string agr = Console.ReadLine();
+            selecteditem = (Item)Enum.Parse(typeof(Item), agr);
+            if (ItemRepository.ItemInstance.ItemCollection[selecteditem] > 0)
+            {
+                UpdateState();
+
+            }
+            else
+            {
+                Console.WriteLine("item not available");
+            }
+        }
+        private void UpdateState()
+        {
+            Vendor.currentState = new GetPrice(this);
+        }
+    }
+    public class GetPrice : VendingMachineState
+    {
+        public GetPrice(VendingMachineState obj)
+        {
+            Vendor = obj.Vendor;
+            selecteditem = obj.selecteditem;
+
+        }
+        public override void DisplayNextScreen()
+        {
+            Console.WriteLine("Pay amount : " + itemsprice);
+          
+            UpdateState();
+        }
+        private void UpdateState()
+        {
+            Vendor.currentState = new PayMoney(this);
+        }
+    }
+    public class PayMoney : VendingMachineState
+    {
+        public PayMoney(VendingMachineState obj)
+        {
+            Vendor = obj.Vendor;
+            AmountPaid = obj.AmountPaid;
+            selecteditem = obj.selecteditem;
+        }
+        public override void DisplayNextScreen()
+        {
+            Console.WriteLine("Enter amount you want to pay : ");
+            string agr = Console.ReadLine();
+            var x = Convert.ToInt32(agr);
+            AmountPaid = AmountPaid + x;
+            if (itemsprice == AmountPaid)
+            {
+                UpdateState(true);
+
+            }
+            else
+            {
+                UpdateState(false);
+               
+            }
+        }
+        private void UpdateState(bool complete)
+        {
+            if (complete)
+                Vendor.currentState = new GetItem(this);
+            else
+            {
+                if(AmountPaid < itemsprice)
+                    Vendor.currentState = new PayRemaining(this);
+                else
+                    Vendor.currentState = new PayRefund(this);
+            }
+
+        }
+    }
+    public class PayRefund : VendingMachineState
+    {
+        public PayRefund(VendingMachineState obj)
+        {
+            Vendor = obj.Vendor;
+            AmountPaid = obj.AmountPaid;
+            selecteditem = obj.selecteditem;
+        }
+        public override void DisplayNextScreen()
+        {
+            Console.WriteLine("Accept refund amount  : " + (AmountPaid - itemsprice).ToString());
+           
+            UpdateState();
+
+        }
+        private void UpdateState()
+        {
+          
+           Vendor.currentState = new GetItem(this);
+         
+        }
+    }
+    public class PayRemaining : VendingMachineState
+    {
+        public PayRemaining(VendingMachineState obj)
+        {
+            Vendor = obj.Vendor;
+            AmountPaid = obj.AmountPaid;
+            selecteditem = obj.selecteditem;
+        }
+        public override void DisplayNextScreen()
+        {
+            Console.WriteLine("Pay remaining amount  : "+(itemsprice - AmountPaid).ToString());
+            Console.WriteLine("Enter amount you want to pay : ");
+            string agr = Console.ReadLine();
+            var x = Convert.ToInt32(agr);
+            AmountPaid = AmountPaid + x;
+            if (itemsprice == AmountPaid)
+            {
+                UpdateState(true);
+
+            }
+            else
+            {
+                UpdateState(false);
+
+            }
+        }
+        private void UpdateState(bool complete)
+        {
+            if (complete)
+                Vendor.currentState = new GetItem(this);
+            else
+            {
+                if (AmountPaid < itemsprice)
+                    Vendor.currentState = new PayRemaining(this);
+                else
+                    Vendor.currentState = new PayRefund(this);
+            }
+
+        }
+    }
+    public class GetItem : VendingMachineState
+    {
+    public GetItem(VendingMachineState obj)
+    {
+        Vendor = obj.Vendor;
+            selecteditem = obj.selecteditem;
+
+        }
+    public override void DisplayNextScreen()
+        {
+            Console.WriteLine("Your transaction is successful for Item "+selecteditem.ToString());
+            UpdateState();
+
+
+        }
+        private void UpdateState()
+        {
+            ItemRepository.ItemInstance.ItemCollection[selecteditem] = ItemRepository.ItemInstance.ItemCollection[selecteditem] - 1;
+            Vendor.currentState = new Selectproduct(this.Vendor);
+        }
+    }
+}
